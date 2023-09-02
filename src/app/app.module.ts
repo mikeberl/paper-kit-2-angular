cbdde520596e423a33b50f58e62561004d0bc85a
Michele Berlanda <mberlanda@unibz.it>
2023-09-02 17:34:44 +0200

    signup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,12 +14,14 @@ import { ExamplesModule } from './examples/examples.module';
 import { NgModule } from '@angular/core';
 import { LoginComponent } from './pages/login/login.component';
 import { CommonModule } from '@angular/common';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -9,15 +9,17 @@ import { SignupComponent } from './examples/signup/signup.component';
 import { LandingComponent } from './examples/landing/landing.component';
 import { NucleoiconsComponent } from './components/nucleoicons/nucleoicons.component';
 import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes =[
-    { path: '', redirectTo: 'home', pathMatch: 'full' },
-    { path: 'home',             component: ComponentsComponent },
+    { path: '', redirectTo: 'xxx', pathMatch: 'full' },
+    { path: 'xxx',             component: ComponentsComponent },
     { path: 'user-profile',     component: ProfileComponent },
     { path: 'signup',           component: SignupComponent },
     { path: 'login',           component: LoginComponent },
     { path: 'landing',          component: LandingComponent },
-    { path: 'nucleoicons',      component: NucleoiconsComponent }
+    { path: 'nucleoicons',      component: NucleoiconsComponent },
+    { path: 'home',      component: HomeComponent }
 ];
 
 @NgModule({
