4cd2582df85141da2e315d00c67c2c3ea26b6206
Michele Berlanda <mberlanda@unibz.it>
2023-09-02 14:34:09 +0200

    services and models from prev project

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,62 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Credentials } from 'app/models/credentials/credentials.module';
+import { AuthService } from 'app/services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+  test : Date = new Date();
+  focus;
+  focus1;
+
+  username: string = '';
+  password: string = '';
+  invalidCredentials: boolean = false;
+  emptyFields: boolean = false;
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  verifyCredentials() {
+    const credentials: Credentials = {
+      username: this.username,
+      password: this.password
+    };
+
+    this.authService.verifyCredentials(credentials).subscribe(
+      response => {
+        console.log(response);
+      },
+      error => {
+        console.log('Errore nella chiamata API');
+      }
+    );
+  }
+
+  login() {
+    if (!this.username || !this.password) {
+      this.emptyFields = true;
+      this.invalidCredentials = false;
+      return;
+    }
+    const credentials: Credentials = {
+      username: this.username,
+      password: this.password
+    };
+    this.authService.verifyCredentials(credentials).subscribe(
+      response => {
+        console.log(response);
+        if (response.result === 'OK') {
+          localStorage.setItem('token', response.token);
+          this.router.navigate(['/home']);
+        } else {
+          this.emptyFields = false;
+          this.invalidCredentials = true;
+        }
+      }
+    );
+  }
+}
