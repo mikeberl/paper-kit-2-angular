ebebd36b933bf304566eea897a7b8c6ddd8f672c
Michele Berlanda <mberlanda@unibz.it>
2023-09-25 16:36:39 +0200

    home refactored

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.setMinHeight();
+  }
+
+  setMinHeight() {
+    const container1 = document.querySelector('.container1') as HTMLElement;
+    const navbar = document.querySelector('.navbar-div') as HTMLElement;
+    const container2 = document.querySelector('.container2') as HTMLElement;
+    
+    if (container1 && navbar) {
+      const navbarHeight = navbar.offsetHeight;
+      const containerHeight = container1.offsetHeight;
+      container2.style.minHeight = `calc(100vh - ${navbarHeight}px - ${containerHeight}px)`;
+    }
+  }
+
   
 }
