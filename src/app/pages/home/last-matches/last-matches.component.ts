527f0b5f4f6bc3cc0e2c49a351d36e4c051dde00
Michele Berlanda <mberlanda@unibz.it>
2023-09-26 12:42:54 +0200

    [DEV] last matches works also in case of <5 matches

--- src/app/pages/home/last-matches/last-matches.component.ts
+++ src/app/pages/home/last-matches/last-matches.component.ts
@@ -6,11 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./last-matches.component.scss']
 })
 export class LastMatchesComponent {
-  bars = [
-    { label: '+15' },
-    { label: '+15' },
-    { label: '+15' },
-    { label: '-15' },
-    { label: '+15' },
-  ];
+  matches: { label: string }[] = [];
+
+  ngOnInit() {
+    // Inizializza la lista matches con almeno 5 elementi
+    while (this.matches.length < 2) {
+      this.matches.push({ label: '+15' }); // Puoi impostare il valore predefinito come desideri
+    }
+    while (this.matches.length >=2 && this.matches.length < 4) {
+      this.matches.push({ label: '-15' }); // Puoi impostare il valore predefinito come desideri
+    }
+    this.matches.push({ label: '' });
+  }
 }
