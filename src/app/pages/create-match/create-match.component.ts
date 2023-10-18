2480db4ceb5690cef3526408b7ab0478047cdcc6
Michele Berlanda <mberlanda@unibz.it>
2023-10-18 18:15:02 +0200

    [ALSO DEV] create page not yet done but 2 cards for winner and losers have been added

--- src/app/pages/create-match/create-match.component.ts
+++ src/app/pages/create-match/create-match.component.ts
@@ -11,43 +11,43 @@ export class CreateMatchComponent {
 
   players = [
     {
+      name: 'Winter 11',
+      imageSrc: 'https://bootdey.com/img/Content/avatar/avatar1.png',
+    },
+    {
+      name: '234 League',
+      imageSrc: 'https://bootdey.com/img/Content/avatar/avatar1.png',
+    },
+    {
+      name: 'xxague',
+      imageSrc: 'https://bootdey.com/img/Content/avatar/avatar1.png',
+    },
+    {
+      name: 'Sumgue',
+      imageSrc: 'https://bootdey.com/img/Content/avatar/avatar1.png',
+    },
+    {
+      name: 'Winnererf dsfsdfsd',
+      imageSrc: 'https://bootdey.com/img/Content/avatar/avatar1.png',
+    },
+    {
+      name: 'Suxccc',
+      imageSrc: 'https://bootdey.com/img/Content/avatar/avatar1.png',
+    },
+    {
+      name: 'Win',
+      imageSrc: 'https://bootdey.com/img/Content/avatar/avatar1.png',
+    },
+    {
+      name: 'Suague',
+      imageSrc: 'https://bootdey.com/img/Content/avatar/avatar1.png',
+    },
+    {
       name: 'Winter League',
       imageSrc: 'https://bootdey.com/img/Content/avatar/avatar1.png',
     },
     {
-      name: 'Summer League',
-      imageSrc: 'https://bootdey.com/img/Content/avatar/avatar1.png',
-    },
-    {
-      name: 'Winter League',
-      imageSrc: 'https://bootdey.com/img/Content/avatar/avatar1.png',
-    },
-    {
-      name: 'Summer League',
-      imageSrc: 'https://bootdey.com/img/Content/avatar/avatar1.png',
-    },
-    {
-      name: 'Winter League',
-      imageSrc: 'https://bootdey.com/img/Content/avatar/avatar1.png',
-    },
-    {
-      name: 'Summer League',
-      imageSrc: 'https://bootdey.com/img/Content/avatar/avatar1.png',
-    },
-    {
-      name: 'Winter League',
-      imageSrc: 'https://bootdey.com/img/Content/avatar/avatar1.png',
-    },
-    {
-      name: 'Summer League',
-      imageSrc: 'https://bootdey.com/img/Content/avatar/avatar1.png',
-    },
-    {
-      name: 'Winter League',
-      imageSrc: 'https://bootdey.com/img/Content/avatar/avatar1.png',
-    },
-    {
-      name: 'Summer League',
+      name: 'Summerague',
       imageSrc: 'https://bootdey.com/img/Content/avatar/avatar1.png',
     },
   ];
@@ -55,8 +55,26 @@ export class CreateMatchComponent {
   searchText: string = '';
   filteredPlayers: any[] = [];
 
+  winners: string[] = [];
+  losers: string[] = [];
+
+  
+
   constructor() {
     this.filteredPlayers = this.players;
+  }
+
+  removeLabel(array: string[], index: number): void {
+    array.splice(index, 1);
+  }
+
+  addToWinners(playerName: string): void {
+    this.winners.push(playerName);
+  }
+
+  // Aggiungi un metodo per aggiungere un giocatore a "losers"
+  addToLosers(playerName: string): void {
+    this.losers.push(playerName);
   }
 
   filterPlayers() {
